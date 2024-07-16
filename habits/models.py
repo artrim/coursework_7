812094bd9635b8edf5d43e9506da06152c51c3a6
Артур Хамидуллин <artrimskiy@gmail.com>
2024-07-16 19:51:49 +0500

    create habit and users apps

---
+++ habits/models.py
@@ -0,0 +1,28 @@
+from datetime import timedelta
+
+from django.db import models
+
+from config import settings
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Habit(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='создатель привычки',
+                              **NULLABLE)
+    place = models.CharField(max_length=150, verbose_name='место выполнения привычки')
+    time = models.TimeField(verbose_name='время выполнения привычки')
+    action = models.TextField(verbose_name='действие, которое представляет собой привычка')
+    pleasant_habit_sign = models.BooleanField(verbose_name='признак приятной привычки', default=False)
+    related_habit = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name='связанная привычка', **NULLABLE)
+    periodicity = models.PositiveSmallIntegerField(verbose_name='переодичность', default=1)
+    award = models.CharField(max_length=150, verbose_name='вознаграждение', **NULLABLE)
+    duration = models.DurationField(verbose_name='продолжительность', default=timedelta(minutes=2))
+    is_published = models.BooleanField(default=True, verbose_name='признак публичности')
+
+    def __str__(self):
+        return f'{self.owner} делает {self.action}, {self.periodicity} раз в неделю'
+
+    class Meta:
+        verbose_name = 'Привычка'
+        verbose_name_plural = 'Привычки'
