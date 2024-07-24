7d1f387512d29dd9a36c3ef726f1a2b90bb58c0d
Артур Хамидуллин <artrimskiy@gmail.com>
2024-07-25 03:21:44 +0500

    create views

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from habits.models import Habit
+from habits.paginators import HabitPagination
+from habits.permissions import IsOwner
+from habits.serializers import HabitSerializers
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    serializer_class = HabitSerializers
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        habit = serializer.save()
+        habit.owner = self.request.user
+        habit.save()
+
+
+class HabitPublishedListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializers
+    pagination_class = HabitPagination
+
+    def get_queryset(self):
+        return Habit.objects.filter(is_published=True)
+
+
+class HabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializers
+    pagination_class = HabitPagination
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Habit.objects.filter(owner=user)
+
+
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = HabitSerializers
+    queryset = Habit.objects.all()
+    permission_classes = [IsOwner]
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = HabitSerializers
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = HabitSerializers
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserListAPIView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
