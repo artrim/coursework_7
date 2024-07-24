47a55ac3a7fbcc0fae52d8889d54a008e6b8fd93
Артур Хамидуллин <artrimskiy@gmail.com>
2024-07-25 02:42:19 +0500

    create admin for habits

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import Habit
+
+
+@admin.register(Habit)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("owner", "place", "time", "action", "pleasant_habit_sign", "related_habit", "periodicity", "award",
+                    "duration", "is_published",)
